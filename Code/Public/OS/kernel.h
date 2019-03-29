50ab53bee737dcb9f607ab33bb39133cd8a516f0
t.k-aria <31005177+TK-Aria@users.noreply.github.com>
2019-03-29 12:56:19 +0900

    プラットフォーム依存部分の修正

--- Code/Public/OS/kernel.h
+++ Code/Public/OS/kernel.h
@@ -7,22 +7,22 @@
 //
 #pragma once
 
-// UNIX OS 
-#ifdef __unix__|__linux__|__FreeBSD__|__NetBSD__ 
-
-#include <Carbon/Carbon.h>
-#include <unistd.h>
-
-#define GetCurrentDirectory getcwd
-#define ChangeDirectory chdir
-
-#else _WIN32
+#ifdef _WIN32
 
 #include <Windows.h>
 
 #undef GetCurrentDirectory
 #define GetCurrentDirectory(dir,size) GetCurrentDirectoryA(size,dir)
 #define ChangeDirectory SetCurrentDirectoryA
+
+// UNIX OS
+#else __unix__|__linux__|__FreeBSD__|__NetBSD__
+
+#include <Carbon/Carbon.h>
+#include <unistd.h>
+
+#define GetCurrentDirectory getcwd
+#define ChangeDirectory chdir
 
 #endif // OS Dependence
 
